c560864dd9f86f7bde0e11acc6963a809f27eafb
Patience-Kori1 <koribiram@yahoo.fr>
2025-07-20 23:53:15 +0200

    feat(controller): create basic controller without AbstractController - Manual HTTP response using the Response object - No use of Twig or render(), for minimalist output - Prepares the ground for learning AbstractController usage

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LuckyController
+{
+    #[Route('/lucky/number')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body> <h1>Lucky number: '.$number.'</h2></body></html>'
+        );
+    }
+}
+
